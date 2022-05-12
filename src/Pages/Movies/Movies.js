a42619bfc51bf430fc2a071b8ce4bfd8ec9ed735
Saurabh Mhaske <mhaskesaurabh3@gmail.com>
2022-05-12 19:53:23 +0530

    Added custom hook useGenre

--- src/Pages/Movies/Movies.js
+++ src/Pages/Movies/Movies.js
@@ -3,16 +3,18 @@ import React, { useEffect, useState } from "react";
 import Genre from "../../Components/Genre";
 import CustomPagination from "../../Components/Pagination/CustomPagination";
 import SingleContent from "../../Components/SingleContent/SingleContent";
+import useGenre from "../../hooks/useGenre";
 
 const Movies = () => {
   const [page, setPage] = useState(1);
   const [content, setContent] = useState([]);
   const [genres, setGenres] = useState([]);
   const [selectedGenres, setSelectedGeneres] = useState([]);
+  const genreforURL = useGenre(selectedGenres);
 
   const fetchMovies = async () => {
     const { data } = await axios.get(
-      `https://api.themoviedb.org/3/discover/movie?api_key=1d203e3a6ccbb77dd4d9d548b01f565f&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=1&with_watch_monetization_types=flatrate&page=${page}`
+      `https://api.themoviedb.org/3/discover/movie?api_key=1d203e3a6ccbb77dd4d9d548b01f565f&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=1&with_watch_monetization_types=flatrate&page=${page}&with_genres=${genreforURL}`
     );
     setContent(data.results);
   };
